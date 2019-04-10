bc56a4dd8ed84a67f660bde876d513008d1dd8d9
Owain Perry <owainperry@statestreet.com>
2019-04-10 20:28:15 +0100

    hardcoded services

--- main.go
+++ main.go
@@ -11,17 +11,26 @@ func main() {
     fmt.Println("Starting the frontend application... 1.0")
 
     servicea := "no results"
+    serviceb := "no results"
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        response, err := http.Get("http://localhost:8080/")
+        response, err := http.Get("http://servicea:8080/")
         if err != nil {
-            fmt.Printf("The HTTP request failed with error %s\n", err)
+            fmt.Printf("The HTTP request failed servicea with error %s\n", err)
         } else {
             data, _ := ioutil.ReadAll(response.Body)
             servicea = string(data)
-        
         }
-        
-        fmt.Fprintf(w, "first service: %s ",servicea)
+        response, err := http.Get("http://serviceb:8080/")
+        if err != nil {
+            fmt.Printf("The HTTP request failed serviceb with error %s\n", err)
+        } else {
+            data, _ := ioutil.ReadAll(response.Body)
+            servicea = string(data)
+        }
+        fmt.Fprintf(w, "<html><body>")
+        fmt.Fprintf(w, "first service: %s <br />",servicea)
+        fmt.Fprintf(w, "second service: %s <br />",serviceb)
+        fmt.Fprintf(w, "</body></html>")
 	})
 	http.ListenAndServe(":9090", nil)
 }
