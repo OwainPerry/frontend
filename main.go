4c144ed46bdbb654a193b65e4a574167f0067d3d
Owain Perry <owainperry@statestreet.com>
2019-04-11 09:16:39 +0100

    added config file

--- main.go
+++ main.go
@@ -4,23 +4,36 @@ import (
 	"fmt"
     "net/http"
     "io/ioutil"
+    "github.com/tkanos/gonfig"
 )
+
+type Configuration struct {
+    Port              int
+    ServiceA   string
+    ServiceB string
+}
 
 func main() {
 
-    fmt.Println("Starting the frontend application... 1.0")
+    configuration := Configuration{}
+    conferr := gonfig.GetConf("config/config.json", &configuration)
+    if conferr != nil {
+        fmt.Println("Failed to read config file")
+    }
+
+    fmt.Printf("Starting the frontend application... 1.3 port : %d",configuration.Port)
 
     servicea := "no results"
     serviceb := "no results"
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        response, err := http.Get("http://servicea:8080/")
+        response, err := http.Get(configuration.ServiceA)
         if err != nil {
             fmt.Printf("The HTTP request failed servicea with error %s\n", err)
         } else {
             data, _ := ioutil.ReadAll(response.Body)
             servicea = string(data)
         }
-        response1, err1 := http.Get("http://serviceb:8080/")
+        response1, err1 := http.Get(configuration.ServiceB)
         if err1 != nil {
             fmt.Printf("The HTTP request failed serviceb with error %s\n", err)
         } else {
@@ -32,5 +45,5 @@ func main() {
         fmt.Fprintf(w, "second service: %s <br />",serviceb)
         fmt.Fprintf(w, "</body></html>")
 	})
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe(fmt.Sprintf(":%d",configuration.Port), nil)
 }
